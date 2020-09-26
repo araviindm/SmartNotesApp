38e9232c9828b1a3b8bc74b99eb521775172028f
araviindm <69687740+araviindm@users.noreply.github.com>
2020-09-26 15:46:36 +0530

    Updated code 7
    Added new post in seperate activity

--- app/src/main/java/com/example/smartnotesapp/HomeFragment.java
+++ app/src/main/java/com/example/smartnotesapp/HomeFragment.java
@@ -3,65 +3,42 @@ package com.example.smartnotesapp;
 import android.content.Intent;
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ImageButton;
 
 
-import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.ChildEventListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
+;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
+
+
 
 
 public class HomeFragment extends Fragment {
 
 
-    EditText editTextTitle,editTextTag,editTextPost;
-    Button postButton;
-
-    DatabaseReference databaseReference;
     FirebaseAuth firebaseAuth;
     private RecyclerView postList;
     PostAdapter adapter;
+    ImageButton addNewPostButton;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View rootView = inflater.inflate(R.layout.fragment_home, container,    false);
 
-        databaseReference = FirebaseDatabase.getInstance().getReference();
-
-        editTextTitle = (EditText) rootView.findViewById(R.id.edit_text_title);
-        editTextTag = (EditText) rootView.findViewById(R.id.edit_text_tag);
-        editTextPost = (EditText) rootView.findViewById(R.id.edit_text_post);
-        postButton = (Button) rootView.findViewById(R.id.edit_text_post_button);
 
         postList = (RecyclerView) rootView.findViewById(R.id.all_users_post_list);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
@@ -74,27 +51,17 @@ public class HomeFragment extends Fragment {
             getActivity().finish();
             startActivity(new Intent(getActivity().getApplicationContext(),LoginActivity.class));
         }
-        final DateFormat dateFormat = new SimpleDateFormat();
-        
-        postButton.setOnClickListener(new View.OnClickListener() {
-            @Override
+        addNewPostButton = rootView.findViewById(R.id.add_new_post_button);
+
+        addNewPostButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                String name = User.name;
-                String title = editTextTitle.getText().toString();
-                String tag = editTextTag.getText().toString();
-                String post = editTextPost.getText().toString();
-                Date time = Calendar.getInstance().getTime();
-                String post_time = dateFormat.format(time);
-
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                String uid = user.getUid();
-
-                Posts newpost = new Posts(uid,name,title,tag,post,post_time);
-                databaseReference.child("Post").push().setValue(newpost);
-                Toast.makeText(getActivity().getApplicationContext(),"posting",Toast.LENGTH_LONG).show();
+                startActivity(new Intent(getActivity(), NewPost.class));
+                getActivity().finish();
             }
         });
+
+        
+
         setUpRecyclerView();
         return rootView;
         
---
+++ app/src/main/java/com/example/smartnotesapp/NewPost.java
@@ -0,0 +1,72 @@
+package com.example.smartnotesapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+public class NewPost extends AppCompatActivity {
+
+    FirebaseAuth firebaseAuth;
+    DatabaseReference databaseReference;
+
+    EditText editTextTitle,editTextTag,editTextPost;
+    Button postButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        editTextTitle = (EditText) findViewById(R.id.edit_text_title);
+        editTextTag = (EditText) findViewById(R.id.edit_text_tag);
+        editTextPost = (EditText) findViewById(R.id.edit_text_post);
+        postButton = (Button) findViewById(R.id.edit_text_post_button);
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        final DateFormat dateFormat = new SimpleDateFormat();
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+        }
+
+            postButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    String name = User.name;
+                    String title = editTextTitle.getText().toString();
+                    String tag = editTextTag.getText().toString();
+                    String post = editTextPost.getText().toString();
+                    Date time = Calendar.getInstance().getTime();
+                    String post_time = dateFormat.format(time);
+
+                    FirebaseUser user = firebaseAuth.getCurrentUser();
+                    String uid = user.getUid();
+
+                    Posts newpost = new Posts(uid,name,title,tag,post,post_time);
+                    databaseReference.child("Post").push().setValue(newpost);
+                    Toast.makeText(getApplicationContext(),"posting",Toast.LENGTH_LONG).show();
+                    startActivity(new Intent(getApplicationContext(),HomeActivity.class));
+
+                }
+            });
+        }
+    }
