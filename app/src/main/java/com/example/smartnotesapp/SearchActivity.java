558546635416565ee600eb3a1e7ebc302aed69e1
araviindm <69687740+araviindm@users.noreply.github.com>
2020-10-26 11:01:50 +0530

    Updated code 12

--- app/src/main/java/com/example/smartnotesapp/SearchActivity.java
+++ app/src/main/java/com/example/smartnotesapp/SearchActivity.java
@@ -1,5 +1,6 @@
 package com.example.smartnotesapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -11,15 +12,24 @@ import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+
 
 import static com.example.smartnotesapp.FollowArray.followArrayList;
 
@@ -31,6 +41,10 @@ public class SearchActivity extends AppCompatActivity {
     PostAdapter adapter;
     EditText searchQuery;
     ImageButton searchButton;
+    LinearLayout followLinearLayout;
+    Button followButtonInSearch;
+    FirebaseUser firebaseUser;
+    TextView tagView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -44,21 +58,25 @@ public class SearchActivity extends AppCompatActivity {
 
         searchQuery = findViewById(R.id.search_query);
         searchButton = findViewById(R.id.search_image_button);
+        followLinearLayout = findViewById(R.id.follow_LinearLayout);
+        followButtonInSearch = findViewById(R.id.follow_button_in_search);
+        tagView = findViewById(R.id.tag_view);
+
         databaseReference = FirebaseDatabase.getInstance().getReference();
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
-        firebaseAuth= FirebaseAuth.getInstance();
-
-        if (firebaseAuth.getCurrentUser() == null){
+        if (firebaseAuth.getCurrentUser() == null) {
             finish();
-            startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+            startActivity(new Intent(getApplicationContext(), LoginActivity.class));
         }
         searchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-               setUpRecyclerView();
-               databaseReference.child("following").setValue(followArrayList);
-               adapter.startListening();
+                setUpRecyclerView();
+                followLinearLayout.setVisibility(LinearLayout.VISIBLE);
+                adapter.startListening();
             }
         });
         searchQuery.setOnEditorActionListener(new TextView.OnEditorActionListener() {
@@ -66,9 +84,16 @@ public class SearchActivity extends AppCompatActivity {
             public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
                 if (actionId == EditorInfo.IME_ACTION_SEARCH) {
                     setUpRecyclerView();
+                    followLinearLayout.setVisibility(LinearLayout.VISIBLE);
                     adapter.startListening();
                 }
                 return false;
+            }
+        });
+        followButtonInSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //databaseReference.child("following").child(firebaseUser.getUid()).setValue();
             }
         });
     }
@@ -78,6 +103,7 @@ public class SearchActivity extends AppCompatActivity {
         in.hideSoftInputFromWindow(searchQuery.getWindowToken(), 0);
         String searchText = searchQuery.getText().toString();
         searchText = searchText.toLowerCase();
+        tagView.setText(searchText);
         Query query = databaseReference.child("Post").orderByChild("tag").equalTo(searchText);
 
         FirebaseRecyclerOptions<Posts> options = new FirebaseRecyclerOptions.Builder<Posts>()
