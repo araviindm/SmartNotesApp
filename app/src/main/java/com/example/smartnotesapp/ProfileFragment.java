4c797a18324e8891e957d516cf10febb207e43b8
araviindm <69687740+araviindm@users.noreply.github.com>
2020-11-04 10:40:09 +0530

    Update 14
    Features completed

--- app/src/main/java/com/example/smartnotesapp/ProfileFragment.java
+++ app/src/main/java/com/example/smartnotesapp/ProfileFragment.java
@@ -1,6 +1,7 @@
 package com.example.smartnotesapp;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -10,27 +11,35 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.squareup.picasso.Picasso;
 
 
 public class ProfileFragment extends Fragment {
 
     DatabaseReference databaseReference;
     FirebaseAuth firebaseAuth;
+    FirebaseStorage firebaseStorage;
     private RecyclerView postList;
     PostAdapter adapter;
+    TextView firstName, lastName;
+    ImageView profilePicImageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_profile, container, false);
-        databaseReference = FirebaseDatabase.getInstance().getReference();
 
         postList =  rootView.findViewById(R.id.all_users_post_list);
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
@@ -38,13 +47,25 @@ public class ProfileFragment extends Fragment {
         postList.setLayoutManager(linearLayoutManager);
         postList.setAdapter(null);
 
-
+        databaseReference = FirebaseDatabase.getInstance().getReference();
         firebaseAuth= FirebaseAuth.getInstance();
+        firebaseStorage = FirebaseStorage.getInstance();
+        StorageReference storageReference = firebaseStorage.getReference();
         if (firebaseAuth.getCurrentUser() == null){
             getActivity().finish();
             startActivity(new Intent(getActivity().getApplicationContext(),LoginActivity.class));
         }
-
+        firstName = rootView.findViewById(R.id.firstName);
+        lastName = rootView.findViewById(R.id.lastName);
+        profilePicImageView = rootView.findViewById(R.id.profileImageView);
+        firstName.setText(User.name);
+        lastName.setText(User.surName);
+        storageReference.child(firebaseAuth.getUid()).child("Images").child("Profile Pic").getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+            @Override
+            public void onSuccess(Uri uri) {
+                Picasso.get().load(uri).fit().centerInside().into(profilePicImageView);
+            }
+        });
         setUpRecyclerView();
         return rootView;
     }
